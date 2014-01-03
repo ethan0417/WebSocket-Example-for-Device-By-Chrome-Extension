76f33a8fee0af5c71bf7b6efea24632a7eb41046
Ethan <ethan.mail0417@gmail.com>
2014-01-03 14:44:28 +0800

    update QR code to create process.

--- javascripts/background.js
+++ javascripts/background.js
@@ -1,10 +1,17 @@
 var server = "211.78.254.40:9999";
 var prefix = "/multiplex";
-var wsUri =  'http://'+ server + prefix;
+var serverURL = 'http://'+server;
+var wsUri =  serverURL + prefix;
+var QRimgURL;
+var QRimgSrc;
 
-function init(roomName) {
-  console.log('start init function!');
-  testWebSocket(roomName);
+function init() {
+  var roomID = randomFn();
+  getRoom(roomID, function(){
+    testWebSocket(roomID);
+    QRimgURL = serverURL +'/'+roomID+'/client';
+    QRimgSrc = QRCode(QRimgURL , 240, 240);
+  });
 }
 /**
  * Connect Function 
@@ -14,23 +21,62 @@ function testWebSocket(roomName) {
   var multiplexer = new WebSocketMultiplex(websocket);
   var ann  = multiplexer.channel(roomName);
   ann.onopen = function (evt) {
-    console.log('open websocket success!!')
     onOpen(evt)
   };
   ann.onclose = function (evt) {
-    console.log('close websocket success!!')
     onClose(evt)
   };
   ann.onmessage = function (evt) {
     onMessage(evt)
   };
   ann.onerror = function (evt) {
-    console.log('websocket error!!')
     onError(evt)
   };
 }
 /**
- *  Function
+ *  Random room ID
+ */
+function randomFn(){
+  var maxNum = 9,
+      minNum = 0,
+      ID = '';
+  for( var i = 0; i< 4; i++){
+    var n = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum,
+    ID = ID + n;
+  };
+  return ID;
+}
+/**
+ *  QR code Fn
+ */
+function QRCode(content, width, height){
+  width = !!width ? width : 240;
+  height = !!height ? height : 240;
+  content = encodeURIComponent(content);
+  return 'http://chart.apis.google.com/chart?cht=qr&chl=' + content + '&chs=' + width + 'x' + height;
+}
+
+/**
+ *  Create service room
+ */
+function getRoom(id, callback){
+  // Random Room ID
+  $.ajax({
+    url: serverURL + '/' + id,
+    success: function(data){
+      if(data.status){
+        callback();
+      }else{
+        alert('crate room error');
+      };
+    },
+    error: function(data){
+      alert('error  :' + data)
+    }
+  })
+}
+/**
+ *  Socket handler function
  */
 function onOpen(evt) {
 }
@@ -46,8 +92,6 @@ function onMessage(evt) {
   getHeight = Number(tmp[1]);
   // Send server X and Y data to contentScript.js
   chrome.tabs.query({active:true, currentWindow:true}, function(tab){
-    console.log('background send message to contentScript : ')
-    console.log({ x: getWidth , y: getHeight })
     chrome.tabs.sendMessage(tab[0].id, { x: getWidth , y: getHeight });
   });
 }
@@ -60,7 +104,6 @@ function openOrFocusOptionsPage() {
     for (var i=0; i < extensionTabs.length; i++) {
       if (optionsUrl == extensionTabs[i].url) {
         found = true;
-        console.log("tab id: " + extensionTabs[i].id);
         chrome.tabs.update(extensionTabs[i].id, {"selected": true});
       };
     };
@@ -69,7 +112,8 @@ function openOrFocusOptionsPage() {
     };
   });
 }
-//chrome.browserAction.onClicked.addListener(init);
+
 chrome.browserAction.onClicked.addListener(function(tab){
   openOrFocusOptionsPage();
+  init();
 });
--- javascripts/options.js
+++ javascripts/options.js
@@ -1,45 +1,7 @@
 $(function(){
   var background = chrome.extension.getBackgroundPage(),
-      serverURL = 'http://' + background.server;
-  // Click Even handler
-  $('input[type=button]').on('click', function(e){
-    console.log(e.target.id);
-    var id = e.target.id;
-    var name = $('#roomName').val();
-    switch (id){
-      case 'start':
-        getRoom(name);
-        break;
-      case 'close':
-        // TODO: 按下結束按鈕時，結束socket連線。
-        break;
-    };
-  });
-  // Greate WebSocked Room
-  function getRoom(id){
-    $.ajax({
-      url: serverURL + '/' + id,
-      success: function(data){
-        if(data.status){
-          background.init(id);
-        }else{
-          $('#error').html('開房時錯誤，請重新整理頁面再按開始！')
-        };
-      },
-      error: function(data){
-        $('#error').html(data);
-      }
-    })
-  }
-  /*
-  // Random Room ID
-  var maxNum = 9,
-      minNum = 0,
-      roomID = '';
-  for( var i = 0; i< 4; i++){
-    var n = Math.floor(Math.random() * (maxNum - minNum + 1)) + minNum,
-    roomID = roomID + n;
-  };
-  */
+      qrImg = background.QRimgSrc,
+      qrUrl = background.QRimgURL;
+  $('#QRCode').attr('src', qrImg);
+  $('#clientURL').html(qrUrl);
 });
-//document.addEventListener('DOMContentLoaded', init);
